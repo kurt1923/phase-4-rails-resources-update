c05c1639a1a8e98c292fe475f2062e397ad83203
Kurt Vermillion <kurtv0727@gmail.com>
2023-02-17 17:54:04 -0500

    rails update

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,15 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  def update
+    bird = bird.find_by(id: params[:id])
+    if bird
+        bird.update(bird_params)
+        render json: bird
+    else
+        render json: { error: 'Bird not found' }, status: :not_found
+    end
+  end
   # POST /birds
   def create
     bird = Bird.create(bird_params)
@@ -22,10 +31,21 @@ class BirdsController < ApplicationController
     end
   end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
